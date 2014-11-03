95e7d0b515894118ac98cbbee010f8a8cb3e353a
gtugablue <gtugablue@gmail.com>
2014-11-03 16:06:46 +0000

    fhfgh

--- codigo/Headers/Particular.h
+++ codigo/Headers/Particular.h
@@ -15,7 +15,7 @@ private:
 public:
 	Particular();
 	Particular(string nome, string utilizador, string password);
-	void showparticulares();
+	void show();
 	//Particular(Data dataNascimento);
 	//Data getDataNascimento() const;
 	//Data setDataNascimento(const Data &data);
