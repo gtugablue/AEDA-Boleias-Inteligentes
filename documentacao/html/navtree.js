faf96012ff29d6e66db23d727dc895d630391f5b
gtugablue <gtugablue@gmail.com>
2014-11-09 17:23:57 +0000

    Doxygen em português

--- documentacao/html/navtree.js
+++ documentacao/html/navtree.js
@@ -2,21 +2,21 @@ var NAVTREE =
 [
   [ "AEDABoleiasInteligentes", "index.html", [
     [ "Classes", null, [
-      [ "Class List", "annotated.html", "annotated" ],
-      [ "Class Index", "classes.html", null ],
-      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
-      [ "Class Members", "functions.html", [
-        [ "All", "functions.html", "functions_dup" ],
-        [ "Functions", "functions_func.html", "functions_func" ],
-        [ "Variables", "functions_vars.html", null ],
-        [ "Related Functions", "functions_rela.html", null ]
+      [ "Lista de componentes", "annotated.html", "annotated" ],
+      [ "Índice dos componentes", "classes.html", null ],
+      [ "Hierarquia de classes", "hierarchy.html", "hierarchy" ],
+      [ "Componentes membro", "functions.html", [
+        [ "Tudo", "functions.html", "functions_dup" ],
+        [ "Funções", "functions_func.html", "functions_func" ],
+        [ "Variáveis", "functions_vars.html", null ],
+        [ "Funções associadas", "functions_rela.html", null ]
       ] ]
     ] ],
-    [ "Files", null, [
-      [ "File List", "files.html", "files" ],
-      [ "File Members", "globals.html", [
-        [ "All", "globals.html", null ],
-        [ "Functions", "globals_func.html", null ],
+    [ "Ficheiros", null, [
+      [ "Lista de ficheiros", "files.html", "files" ],
+      [ "Ficheiros membro", "globals.html", [
+        [ "Tudo", "globals.html", null ],
+        [ "Funções", "globals_func.html", null ],
         [ "Macros", "globals_defs.html", null ]
       ] ]
     ] ]
