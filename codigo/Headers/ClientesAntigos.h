125bef21598eeebcb9363f27035b59edc24e2948
Pedro <pedro.mvsilva@gmail.com>
2015-01-03 16:00:00 +0000

    nova classe

---
+++ codigo/Headers/ClientesAntigos.h
@@ -0,0 +1,15 @@
+#ifndef _CLIENTES_ANTIGOS_
+#define _CLIENTES_ANTIGOS
+
+#include <vector>
+#include <string>
+
+#include "Boleia.h"
+
+class ClientesAntigos
+{
+	vector<Boleia*>registos;
+	Particular* cliente;
+};
+
+#endif
